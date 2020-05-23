143b7f555c046a333a3a53c57fa91439bb83eb1f
InanKaya <inan.kaya35@hotmail.com>
2020-05-23 21:30:28 +0300

    ss
    ss

--- src/main/java/SparkRDDWorldCup/App.java
+++ src/main/java/SparkRDDWorldCup/App.java
@@ -1,43 +1,47 @@
-package SparkRDDWorldCup;
+package spark.rdd.WorldCount;
 
 import com.google.common.collect.Iterators;
-import com.mongodb.spark.sql.SparkSessionFunctions;
-import org.apache.spark.SparkContext;
+import com.mongodb.spark.MongoSpark;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.SparkSession;
+import org.bson.Document;
 import scala.Tuple2;
 
-import javax.swing.text.Document;
 import java.util.Iterator;
+
 
 public class App {
     public static void main(String[] args) {
-        System.setProperty("hadoop.home.dir","D:\\hadoop-2.7.1-bin");
+        System.setProperty("hadoop.home.dir", "D:\\hadoop-2.7.1-bin");
 
+        SparkSession spark= SparkSession.builder()
+                .master("local")
+                .appName("MongoSpark")
+                .config("spark.mongodb.input.uri","mongodb://127.0.0.1/test.WordCupCollection")
+                .config("spark.mongodb.output.uri","mongodb://127.0.0.1/test.WordCupCollection")
+                .getOrCreate();
+        JavaSparkContext sc=new JavaSparkContext(spark.sparkContext());
 
-        SparkContext sparkContext = new SparkContext();
-        SparkSessionFunctions
-
-        JavaSparkContext sc = new JavaSparkContext("local","WorldCupApp");
         JavaRDD<String> Raw_Data = sc.textFile("C:\\Users\\inan\\Desktop\\WorldCup\\WorldCupPlayers.csv");
 
         JavaRDD<PlayersModel> playersRDD = Raw_Data.map(new Function<String, PlayersModel>() {
-
-            public PlayersModel call(String s) throws Exception {
-                String[] dizi = s.split(",");
-
-                return new PlayersModel(dizi[0], dizi[1], dizi[2], dizi[3], dizi[4], dizi[5], dizi[6], dizi[7], dizi[8]);
+            public PlayersModel call(String line) throws Exception {
+                String[] dizi = line.split(",",-1);
+                return new PlayersModel(dizi[0], dizi[1]
+                        , dizi[2], dizi[3], dizi[4], dizi[5], dizi[6], dizi[7], dizi[8]);
             }
         });
+
         JavaRDD<PlayersModel> tur = playersRDD.filter(new Function<PlayersModel, Boolean>() {
             public Boolean call(PlayersModel playersModel) throws Exception {
                 return playersModel.getTeam().equals("TUR");
             }
         });
+
 
         JavaPairRDD<String, String> mapRDD = tur.mapToPair(new PairFunction<PlayersModel, String, String>() {
             public Tuple2<String, String> call(PlayersModel playersModel) throws Exception {
@@ -55,13 +59,6 @@ public class App {
             }
         });
 
-
-        resultRDD.foreach(new VoidFunction<SparkRDDWorldCup.groupPlayer>() {
-            public void call(groupPlayer groupPlayer) throws Exception {
-                System.out.println(groupPlayer.getPlayerName() + " " + groupPlayer.getMatchCount());
-            }
-        });
-
         JavaRDD<Document> MongoRDD = resultRDD.map(new Function<groupPlayer, Document>() {
             public Document call(groupPlayer groupPlayer) throws Exception {
                 return Document.parse("{PlayerName: " + " ' " + groupPlayer.getPlayerName() + "'"
@@ -69,5 +66,8 @@ public class App {
                         +"}");
             }
         });
+
+        MongoSpark.save(MongoRDD);
+
     }
 }
